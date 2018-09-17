fb650a6117cb2b31ae3ea5d4562473daefa87ef6
matthew <matthewtranov@gmail.com>
2018-09-17 12:43:23 -0400

    second commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Hello World!");
+            Console.Write("test");
             Console.ReadKey();
         }
     }
