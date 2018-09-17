2289eddab81db37c9b45bbd4c941e08b97e5f16d
matthew <matthewtranov@gmail.com>
2018-09-17 13:23:43 -0400

    added display grip

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,14 +4,33 @@ namespace ConsoleApp1
 {
     class Program
     {
+        public bool running = true;
+        public bool[,] grid = new bool[24, 10];
+        
+
+        public void display()
+        {
+            for(int i=0; i < grid.GetLength(0); i++)
+            {
+                for(int j=0; j < grid.GetLength(1);j++)
+                {
+                    if (grid[i,j])
+                        Console.Write("o");
+                    else
+                        Console.Write("x");
+                }
+                Console.WriteLine("");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.Write("Hello World!");
+            Program game = new Program();
+            game.grid[1, 1] = true;
             Console.Clear();
-            Console.WriteLine("oooooooooo");
-            Console.WriteLine("xxxxxxxxxx");
-            Console.WriteLine("oxoxoxoxox");
+            game.display();
             Console.ReadKey();
         }
+
     }
 }
